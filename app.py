b2936c0b7f76443a2da625f02033dbf26465a591
Ponder Stibbons <ponder.stibbons.uu.am@gmail.com>
2023-02-25 19:33:05 +0100

    Fix Lint and unit tests (utf-8)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, make_response
 APP = Flask(__name__)
 
 # Load the data
-MASCOTS = json.load(open('data.json', 'r'))
+MASCOTS = json.load(open('data.json', 'r', encoding="utf-8"))
 
 
 @APP.route('/', methods=['GET'])
